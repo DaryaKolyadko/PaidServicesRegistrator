c2cfbb57d665783aba5c572934145a7c45256f41
darya_kolyadko <skvoznak1996@gmail.com>
2016-03-22 21:49:26 +0300

    Creaate skeleton project

---
+++ PaidServicesRegistrator/PaidServicesRegistrator/WebRegistrationService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebRegistrationService.asmx.cs" Class="PaidServicesRegistrator.WebRegistrationService" %>
---
+++ PaidServicesRegistrator/PaidServicesRegistrator/WebRegistrationService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace PaidServicesRegistrator
+{
+    /// <summary>
+    /// Сводное описание для WebRegistrationService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebRegistrationService : WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Привет всем!";
+        }
+    }
+}
